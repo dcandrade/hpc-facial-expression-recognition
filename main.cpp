cf4d26b39e84466021c53e118c869c98a017f929
Daniel C. Andrade <dca650@gmail.com>
2018-10-10 11:18:04 -0300

    refactoring and plotting graphs

--- main.cpp
+++ main.cpp
@@ -13,25 +13,25 @@ int NUM_TRAIN_OBSERVATIONS = 3995 + 4097 + 1; //emotion 0 + emotion 2
 int NUM_TEST_OBSERVATIONS = 958 + 1054 + 1;
 int NUM_EPOCHS = 100;
 int COST_THRESHOLD = 0.001;
-double LEARNING_RATE = 0.01;
+float LEARNING_RATE = 0.01;
 
-double **allocMatrix(int rows, int cols){
-    double **matrix = (double **)malloc(rows * sizeof(double *)); 
+float **allocMatrix(int rows, int cols){
+    float **matrix = (float **)malloc(rows * sizeof(float *)); 
     for (int i=0; i<rows; i++) 
-         matrix[i] = (double *)malloc(cols * sizeof(double)); 
+         matrix[i] = (float *)malloc(cols * sizeof(float)); 
 
     return matrix;
 }
 
-void freeMatrix(double **matrix, int rows, int cols){
+void freeMatrix(float **matrix, int rows, int cols){
     for(int i=0; i<rows; i++) 
         free(matrix[i]);
     free(matrix);
 }
 
-void parsePixels(string pixels_str, double *pixels){
+void parsePixels(string pixels_str, float *pixels){
     stringstream ss(pixels_str);
-    double pixel;
+    float pixel;
     int i=0;
     
     while(ss >> pixel){
@@ -40,7 +40,7 @@ void parsePixels(string pixels_str, double *pixels){
     }
 }
 
-void addToDataset(double **X, double *y, int index, int emotion, double* pixels, string usage){
+void addToDataset(float **X, float *y, int index, int emotion, float* pixels, string usage){
     X[index][0] = 1;
     y[index] = emotion;
 
@@ -49,22 +49,12 @@ void addToDataset(double **X, double *y, int index, int emotion, double* pixels,
     }
 }
 
-void initWeights(double *weights){
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator (seed);
-    uniform_real_distribution<> distribution(0.0001, 1); // min = 0, max = 1
-
-    for(int i = 0; i < NUM_FEATURES; i++){
-        weights[i] = 0;//distribution(generator);
-    }
-}
-
-double sigmoid(double z){
+float sigmoid(float z){
     return 1.0/(1.0+exp(-z));
 }
 
-double hypothesis(double *weights, double *observation){ //observation == xi
-    double z = 0;
+float hypothesis(float *weights, float *observation){ //observation == xi
+    float z = 0;
 
     for (int i = 0; i < NUM_FEATURES; i++){
         z += (weights[i] * observation[i]);
@@ -73,24 +63,24 @@ double hypothesis(double *weights, double *observation){ //observation == xi
     return sigmoid(z);
 }
 
-double cost_function(double **X_train, double *y_train, double *predictions){
-    double cost = 0;
-    double h_xi;
+float cost_function(float **X_train, float *y_train, float *predictions){
+    float cost = 0;
+    float h_xi;
 
     for (int i = 0; i < NUM_TRAIN_OBSERVATIONS; i++){
         h_xi = predictions[i];
-        double p1 = y_train[i] * log(h_xi);
-        double p2 = (1-y_train[i]) * log(1-h_xi);
+        float p1 = y_train[i] * log(h_xi);
+        float p2 = (1-y_train[i]) * log(1-h_xi);
         cost += (-p1-p2);
     }
 
     return  cost/NUM_TRAIN_OBSERVATIONS;
 }
 
-double gradient(double **X_train, double *y_train, double *predictions, int j){
-    double h_xi = 0;
-    double *xi;
-    double sum = 0;
+float gradient(float **X_train, float *y_train, float *predictions, int j){
+    float h_xi = 0;
+    float *xi;
+    float sum = 0;
     
     for(int i = 0; i < NUM_TRAIN_OBSERVATIONS; i++){
         xi = X_train[i];
@@ -100,16 +90,15 @@ double gradient(double **X_train, double *y_train, double *predictions, int j){
     return (LEARNING_RATE/NUM_TRAIN_OBSERVATIONS) * sum;
 }
 
-void updateWeights(double **X_train, double *y_train, double *weights, double *predictions){
-    
+void updateWeights(float **X_train, float *y_train, float *weights, float *predictions){    
     for(int j = 0; j < NUM_FEATURES; j++){
 	    weights[j] -= gradient(X_train, y_train, predictions, j);
     }
 }
 
-void saveEpoch(int epoch, ofstream &outputFile, double *predictions, double *y, int size, double cost, long time){
-    double accuracy, precision, recall, f1;
-    double tp = 0, tn = 0, fp = 0, fn = 0;
+void saveEpoch(int epoch, ofstream &outputFile, float *predictions, float *y, int size, float cost, long time){
+    float accuracy, precision, recall, f1;
+    float tp = 0, tn = 0, fp = 0, fn = 0;
     int pred, real;
 
     for(int i = 0; i < size; i++){
@@ -136,29 +125,29 @@ void saveEpoch(int epoch, ofstream &outputFile, double *predictions, double *y, 
 
 void parse_args(int argc, char**argv){
     if(argc < 4){
-        //cerr<< "Utilizando configuração de parâmetros padrão"<<endl;
         return;
     }
+
     NUM_TRAIN_OBSERVATIONS = atof(argv[0]) + 1;
     NUM_TEST_OBSERVATIONS = atof(argv[1]) + 1;
     NUM_EPOCHS = atof(argv[2]);
-    //COST_THRESHOLD = atof(argv[3]);
     LEARNING_RATE = atof(argv[4]);
 }
 
 int main(int argc, char** argv){
     parse_args(argc, argv);
 
-    double **X_train, **X_test, *weights, *newWeights;
-    double *y_train, *y_test;
+    float **X_train, **X_test, *weights, *newWeights;
+    float *y_train, *y_test;
     X_train = allocMatrix(NUM_TRAIN_OBSERVATIONS, NUM_FEATURES);
     X_test = allocMatrix(NUM_TEST_OBSERVATIONS, NUM_FEATURES);
-    y_train = (double *)malloc(NUM_TRAIN_OBSERVATIONS * sizeof(double));
-    y_test = (double *)malloc(NUM_TEST_OBSERVATIONS * sizeof(double));
+    y_train = (float *)malloc(NUM_TRAIN_OBSERVATIONS * sizeof(float));
+    y_test = (float *)malloc(NUM_TEST_OBSERVATIONS * sizeof(float));
 
-    weights = (double *) malloc (NUM_FEATURES * sizeof(double));
-    newWeights = (double *) malloc (NUM_FEATURES * sizeof(double));
-    initWeights(weights);
+    weights = (float *) malloc (NUM_FEATURES * sizeof(float));
+    for(int i = 0; i < NUM_FEATURES; i++){
+        weights[i] = 0;//distribution(generator);
+    }
 
     ifstream inputFile;
     ofstream outputFile;
@@ -166,19 +155,19 @@ int main(int argc, char** argv){
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     string outputFileName = "results/output_" +to_string(NUM_EPOCHS) + "epochs_" + to_string(NUM_TRAIN_OBSERVATIONS) + "train_" + to_string(NUM_TEST_OBSERVATIONS) + "test___"+to_string(seed)+".txt";
     outputFile.open(outputFileName);
-    outputFile << "epoch,accuracy,precision,recall,f1,cost,time" << endl;
+    outputFile << "epoch,accuraloadscy,precision,recall,f1,cost,time" << endl;
     string line;
     
     int index_train = 0, index_test = 0;
-    double pixels[48*48];
+    float pixels[48*48];
     getline(inputFile, line); // skip first line
     
     // Reading input
     while(getline(inputFile, line)){
-        int lastPixelIndex = line.find(",", 2);
         int emotion = line[0] - '0';
         
         if(emotion == 0 || emotion == 2){
+                int lastPixelIndex = line.find(",", 2);
                 string pixels_str = line.substr(2, line.find(",", 2)-2);
                 string usage = line.substr(lastPixelIndex+1);
                 parsePixels(pixels_str, pixels);
@@ -197,7 +186,7 @@ int main(int argc, char** argv){
     
     int epoch = 1;
     while(epoch <= NUM_EPOCHS){
-        double predictions[NUM_TRAIN_OBSERVATIONS], pred;
+        float predictions[NUM_TRAIN_OBSERVATIONS], pred;
         auto start = chrono::system_clock::now();
 
         for (int i = 0; i < NUM_TRAIN_OBSERVATIONS; i++){
@@ -209,14 +198,14 @@ int main(int argc, char** argv){
 
         auto end = chrono::system_clock::now();
         long elapsed = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        double cost = cost_function(X_train, y_train, predictions);
+        float cost = cost_function(X_train, y_train, predictions);
         
         saveEpoch(epoch, outputFile, predictions, y_train, NUM_TRAIN_OBSERVATIONS, cost, elapsed);
         epoch ++;
     }
 
     // Run tests
-    double predictions[NUM_TEST_OBSERVATIONS], pred;
+    float predictions[NUM_TEST_OBSERVATIONS], pred;
     int correct = 0;
     for (int i = 0; i < NUM_TEST_OBSERVATIONS; i++){
         pred = hypothesis(weights, X_test[i]);
